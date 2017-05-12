c9b56dcd846028c78634afeee9406169be5d940b
Ega Prathama <ega.prathama@ti.ukdw.ac.id>
2017-05-13 03:45:23 +0700

    icon fa, ganti icon di contact

--- resources/views/contact/contact.blade.php
+++ resources/views/contact/contact.blade.php
@@ -41,28 +41,28 @@
             <div class="row four-column">
                 <div class="col-md-3 col-sm-6 col-xs-12 sm-margin-nine-bottom xs-margin-thirteen-bottom text-center sm-clear-both">
                     <div class="feature-box xs-margin-thirteen xs-no-margin-tb hvr-buzz-out">     
-                        <i class="fa ti-location-pin text-sky-blue icon-large tz-icon-color margin-ten-bottom xs-margin-five-bottom"></i>
-                        <h3 class="feature-title text-white text-medium font-weight-400 alt-font display-block margin-three-bottom tz-text">Contact Address</h3>
+                        <i class="fa fa-instagram text-sky-blue icon-large tz-icon-color margin-ten-bottom xs-margin-five-bottom"></i>
+                        <h3 class="feature-title text-white text-medium font-weight-400 alt-font display-block margin-three-bottom tz-text">Instafram</h3>
                         <div class="text-medium center-col tz-text">301 The Greenhouse, Custard,<br>Factory, London, E2 8DY.</div>
                     </div>
                 </div>
                 <div class="col-md-3 col-sm-6 col-xs-12 sm-margin-nine-bottom xs-margin-thirteen-bottom text-center">
                     <div class="feature-box xs-margin-thirteen xs-no-margin-tb hvr-buzz-out">
-                        <i class="fa ti-mobile text-sky-blue icon-large tz-icon-color margin-ten-bottom xs-margin-five-bottom"></i>
-                        <h3 class="feature-title text-white text-medium font-weight-400 alt-font display-block margin-three-bottom tz-text">Call Us Today!</h3>
+                        <i class="fa fa-whatsapp text-sky-blue icon-large tz-icon-color margin-ten-bottom xs-margin-five-bottom"></i>
+                        <h3 class="feature-title text-white text-medium font-weight-400 alt-font display-block margin-three-bottom tz-text">Whatsapp</h3>
                         <div class="text-medium center-col tz-text">(M) +44 (0) 123 456 7890<br>(O) +44 (0) 123 456 7890</div>
                     </div>
                 </div>
                 <div class="col-md-3 col-sm-6 col-xs-12 text-center xs-margin-thirteen-bottom sm-clear-both">
                     <div class="feature-box xs-margin-thirteen xs-no-margin-tb hvr-buzz-out">
-                        <i class="fa ti-email text-sky-blue icon-large tz-icon-color margin-ten-bottom xs-margin-five-bottom"></i>
+                        <i class="fa fa-group text-sky-blue icon-large tz-icon-color margin-ten-bottom xs-margin-five-bottom"></i>
                         <h3 class="feature-title text-white text-medium font-weight-400 alt-font display-block margin-three-bottom tz-text">Email</h3>
                         <div class="text-medium center-col"><a class="tz-text text-light-gray2" href="mailto:no-reply@domain.com">no-reply@domain.com</a><br><a class="tz-text text-light-gray2" href="mailto:help@domain.com">help@domain.com</a></div>
                     </div>
                 </div>
                 <div class="col-md-3 col-sm-6 col-xs-12 text-center">
                     <div class="feature-box xs-margin-thirteen xs-no-margin-tb hvr-buzz-out">
-                        <i class="fa ti-time text-sky-blue icon-large tz-icon-color margin-ten-bottom xs-margin-five-bottom"></i>
+                        <i class="fa fa-clock-o text-sky-blue icon-large tz-icon-color margin-ten-bottom xs-margin-five-bottom"></i>
                         <h3 class="feature-title text-white text-medium font-weight-400 alt-font display-block margin-three-bottom tz-text">Working Hours</h3>
                         <div class="text-medium center-col tz-text">Mon to Sat - 9 AM to 11 PM<br>Sunday 10 AM to 6 PM</div>
                     </div>
